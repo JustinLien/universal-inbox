7a50f487fd0a95361618e794270a801343994f36
Justin Lien <lien.justin.m@gmail.com>
2016-07-11 15:32:11 -0700

    config: utility logger and tweet factory
    Port over to production (public folder and index.jade)

--- public/app/main.controller.js
+++ public/app/main.controller.js
@@ -3,7 +3,6 @@
 function MainController(TweetsFactory) {
   const vm = this;
 
-  //<editor-fold desc="Get and display tweets">
   vm.tweets = [];
 
   // Saving this method when getTweets is a real
@@ -12,34 +11,24 @@ function MainController(TweetsFactory) {
     vm.tweets = data;
   });
 
+
   //const tweets = TweetsFactory.getTweets();
 
   // vm.tweets = TweetsFactory.getTweets();
 
   /*tweets.forEach(function(tweet) {
-   console.log('tweet.text: ', tweet.text);
-   console.log('tweet.user.name: ', tweet.user.name);
-   console.log('tweet.user.screen_name: @', tweet.user.screen_name);
-   console.log('tweet.user.profile_image_url: ', tweet.user.profile_image_url);
-   console.log('tweet.user.profile_image_url_https: ', tweet.user.profile_image_url_https);
-   console.log('tweet.created_at: ', tweet.created_at);
-   // console.log('tweet: ', tweet);
-   // console.log('tweet: ', tweet);
+    console.log('tweet.text: ', tweet.text);
+    console.log('tweet.user.name: ', tweet.user.name);
+    console.log('tweet.user.screen_name: @', tweet.user.screen_name);
+    console.log('tweet.user.profile_image_url: ', tweet.user.profile_image_url);
+    console.log('tweet.user.profile_image_url_https: ', tweet.user.profile_image_url_https);
+    console.log('tweet.created_at: ', tweet.created_at);
+    // console.log('tweet: ', tweet);
+    // console.log('tweet: ', tweet);
 
-   });*/
-  //</editor-fold>
 
-  //<editor-fold desc="Filter by completed posts">
-  vm.viewCompleted = false;
+  });*/
 
-  vm.matchesCompletedFilter = function (post) {
-    return post.completedByUser ? vm.viewCompleted : !vm.viewCompleted;
-  };
-
-  //vm.applyCompletedFilter = function (filterStr) {
-  //
-  //}
-  //</editor-fold>
 }
 
 angular
@@ -47,4 +36,3 @@ angular
   .controller('MainController', MainController);
 
 MainController.$inject = ['TweetsFactory'];
-
